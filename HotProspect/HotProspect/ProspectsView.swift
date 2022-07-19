a88b23da5e4d4dbe144ac6dc4f4de2177ee5c64b
mlpdleal <manoellealdeveloper@gmail.com>
2022-07-19 20:38:38 -0300

    Completed Day 83 of 100 Days of SwiftUI

--- HotProspect/HotProspect/ProspectsView.swift
+++ HotProspect/HotProspect/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Manoel Leal on 18/07/22.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -14,6 +15,7 @@ struct ProspectsView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     
     let filter: FilterType
     
@@ -28,18 +30,36 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .swipeActions{
+                        if prospect.isContacted{
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark")
+                            }
+                            .tint(.blue)
+                        } else {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
+                            }
+                            .tint(.green)
+                        }
+                            
+                    }
                 }
             }
                 .navigationTitle(title)
                 .toolbar{
                     Button{
-                        let prospect = Prospect()
-                        prospect.name = "Paul Hudson"
-                        prospect.emailAddress = "paul@hackingwithswift.com"
-                        prospects.people.append(prospect)
+                        isShowingScanner = true
                     } label: {
                         Label("Scan", systemImage: "qrcode.viewfinder")
                     }
+                }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
                 }
                 
         }
@@ -65,6 +85,24 @@ struct ProspectsView: View {
         case .uncontacted:
             return prospects.people.filter { !$0.isContacted }
         }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>){
+        isShowingScanner = false
+        
+        switch result{
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+        
     }
 }
 
