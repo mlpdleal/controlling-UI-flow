2a0066795fb8cc839024dfda6815a3d47d580dac
mlpdleal <manoellealdeveloper@gmail.com>
2022-07-28 22:53:31 -0300

    day 94

---
+++ GeometryChallenge/GeometryChallenge/GeometryChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryChallengeApp.swift
+//  GeometryChallenge
+//
+//  Created by Manoel Leal on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
