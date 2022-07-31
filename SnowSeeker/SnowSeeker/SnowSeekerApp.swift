ab04f9d8962059fe9c134fb57e8b3ca2c226e99e
mlpdleal <manoellealdeveloper@gmail.com>
2022-07-31 20:09:28 -0300

    Day 97

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Manoel Leal on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
